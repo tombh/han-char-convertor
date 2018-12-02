e57e98c8fa6766ae6b5c9e7959d106512499fccf
Thomas Buckley-Houston <tom@tombh.co.uk>
2018-12-02 14:54:59 +0900

    Use ENV's PORT if available

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const KuromojiAnalyzer = require("kuroshiro-analyzer-kuromoji")
 const kuroshiro = new Kuroshiro()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 async function init() {
   await kuroshiro.init(new KuromojiAnalyzer())
